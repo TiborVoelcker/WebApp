1eb931c09cef3944fd030360ef5b7cad65682c23
TiborVoelcker <tiborvoelcker@hotmail.de>
2020-10-20 09:41:16 +0200

    refactor: tweak database models

--- SecretHitler/models.py
+++ SecretHitler/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Game(models.Model):
     codeword = models.CharField(max_length=50)
-    current_president = models.OneToOneField('Player', on_delete=models.CASCADE, related_name='role', null=True)
-    current_chancellor = models.OneToOneField('Player', on_delete=models.CASCADE, related_name='+', null=True)
+    current_president = models.OneToOneField('Player', on_delete=models.CASCADE, related_name='is_president_from', null=True)
+    current_chancellor = models.OneToOneField('Player', on_delete=models.CASCADE, related_name='is_chancellor_from', null=True)
     turn_no = models.IntegerField(default=1)
     current_state = models.CharField(max_length=20, default='pre_game')
     elected_policies = models.CharField(max_length=200, default='[]')
@@ -15,7 +15,7 @@ class Game(models.Model):
         return self.codeword
 
 
-class Player(models.Model):
+class Player(models.Model): # TODO: maybe connect with User model or replace it?
     name = models.CharField(max_length=50)
     game = models.ForeignKey('Game', on_delete=models.SET_NULL, null=True, related_name='players')
     voted = models.BooleanField(default=False)
