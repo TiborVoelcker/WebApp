656a03ca822d684268491423182395495343c769
TiborVoelcker <tiborvoelcker@hotmail.de>
2020-11-18 09:13:47 +0100

    refactor: move code from helper.py to respective module

--- app/main/sockets.py
+++ app/main/sockets.py
@@ -1,15 +1,48 @@
+import functools
 import random
 
 from flask import request, current_app
 from flask_login import current_user
+from flask_socketio import disconnect
 from flask_socketio import emit, join_room, leave_room, close_room
 
 from app import socketio, db
-from app.helper import check_game_state, tuple_to_player, player_to_tuple, authenticated_only
 from app.models import Game
+from app.models import Player
 
 
 # ToDo: change commit to flush? Automatically commit on right times (maybe from model methods?)
+
+def authenticated_only(f):
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        if not current_user.is_authenticated:
+            disconnect()
+        else:
+            return f(*args, **kwargs)
+    return wrapped
+
+
+def check_game_state(state):
+    def wrapper(func):
+        def inner(*args):
+            g = current_user.current_game
+            if g.current_state == state:
+                return func(g, *args)
+            else:
+                return False, 'You cannot do that right now!'
+
+        return inner
+
+    return wrapper
+
+
+def tuple_to_player(player_tuple):
+    return Player.query.get(player_tuple[0])
+
+
+def player_to_tuple(player):
+    return player.id, player.name
 
 
 @socketio.on('connect')
