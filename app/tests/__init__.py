3c7965946077cf67dfa8d1e6a8b2fab074c23bea
TiborVoelcker <tiborvoelcker@hotmail.de>
2020-12-25 17:51:34 +0100

    feat: added all tests to tests/__init__.py

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -1,12 +1,16 @@
 import unittest
 
 from .test_models import TestModels
+from .test_routes import TestRoutes
+from .test_socketio import TestSocketIO
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # load test models
 suite.addTests(loader.loadTestsFromTestCase(TestModels))
+suite.addTests(loader.loadTestsFromTestCase(TestRoutes))
+suite.addTests(loader.loadTestsFromTestCase(TestSocketIO))
 
 runner = unittest.TextTestRunner(verbosity=2)
 
